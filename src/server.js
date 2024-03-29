c72a0c9cffd4ae918616993c6168a8a4fb6ccb57
Dean Kim <deankim1118@gmail.com>
2024-03-29 15:26:16 -0400

    3. Understanding Middleware

--- src/server.js
+++ src/server.js
@@ -4,11 +4,21 @@ const app = express();
 
 const PORT = 4000;
 
+const handleListening = () => console.log(`Server is listening on port localhose:${PORT}`);
+
+// Middleware는 request에 응답하는 함수가 아니라, request를 지연시키고 작업을 다음 함수로 넘겨주는 함수이다.
+const logger = (req, res, next) => {
+  console.log(`Someone requests by ${req.method} ${req.url}`);
+  next();
+} 
+
 const handlehome = (req, res) => {
-    return res.send('This is my first Server')
+  const responseText = `<h1>This is my first server</h1>`
+  return res.send(responseText)
 }
 
-const handleListening = () => console.log(`Server is listening on port localhose:${PORT}`);
+// app.use를 사용하면 모든 Routes 에서 Middleware가 실행된다.
+app.use(logger);
 
 app.get("/", handlehome);
 

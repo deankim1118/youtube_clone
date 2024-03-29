beb10e9e6b5b5baed61ff71c977d3fc03068b098
Dean Kim <deankim1118@gmail.com>
2024-03-29 14:50:23 -0400

    2, Get req, res

--- src/server.js
+++ src/server.js
@@ -1,1 +1,15 @@
-console.log("Hello, world. I'll be the best react developer in the world. Wait. God is with me")
+import express from "express";
+
+const app = express();
+
+const PORT = 4000;
+
+const handlehome = (req, res) => {
+    return res.send('This is my first Server')
+}
+
+const handleListening = () => console.log(`Server is listening on port localhose:${PORT}`);
+
+app.get("/", handlehome);
+
+app.listen(PORT, handleListening)

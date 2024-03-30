d8b2138b45a37ca306bda971f635d99e3a09e6cc
Dean Kim <deankim1118@gmail.com>
2024-03-30 13:24:38 -0400

    6-1. Set Pug to view engine & make views folder

--- src/server.js
+++ src/server.js
@@ -8,10 +8,12 @@ const app = express();
 const logger =  morgan("dev");
 // Middleware는 request에 응답하는 함수가 아니라, request를 지연시키고 작업을 다음 함수로 넘겨주는 함수이다.
 // app.use를 사용하면 모든 Routes 에서 Middleware가 실행된다.
-app.use(logger);
+
 
 const PORT = 4000;
 
+app.set("view engine", "pug")
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);

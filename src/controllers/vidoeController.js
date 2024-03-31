232730fa00176792720fd90fddccd6c0f0cad4e8
Dean Kim <deankim1118@gmail.com>
2024-03-31 19:25:39 -0400

    Practice Pug Mixins

--- src/controllers/vidoeController.js
+++ src/controllers/vidoeController.js
@@ -1,7 +1,31 @@
 
 
 export const trending = (req, res) => { 
-    const videos = [{title: "Hello"}, {title: "vidoe2"}];
+    const videos = [
+        {
+            title: "First Video", 
+            rating: 5, 
+            comments:2, 
+            createAt: "2 minutes ago",
+            views: 89,
+            id: 1,
+        }, 
+        {
+            title: "Second Video", 
+            rating: 4, 
+            comments:21, 
+            createAt: "30 minutes ago",
+            views: 49,
+            id: 2,
+        },
+        {
+            title: "Third Video", 
+            rating: 5, 
+            comments:2, 
+            createAt: "45 minutes ago",
+            views: 89,
+            id: 3,
+        },];
     // render 1번째 Parameter는 "home.pug" view engine의 파일 이름을 넣어준다.
     // render 2번째에 Parameter에 pug에서 사용할 변수를 보내줄 수 있다. ex) {pageTitle: "Home", userDataObject}
     return res.render("home", {pageTitle: "Home", videos});

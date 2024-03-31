951d9c89cc1e214b156720c9cb61ccd5da422a43
Dean Kim <deankim1118@gmail.com>
2024-03-31 17:29:36 -0400

    MVP.css

--- src/controllers/vidoeController.js
+++ src/controllers/vidoeController.js
@@ -1,7 +1,7 @@
-
-export const trending = (req, res) => { res.render("home"); }
-export const see = (req, res) => { res.send("Watch Videos")}
-export const edit = (req, res) => { res.send("Edit Videos")}
+// render 2번째에 Parameter에 pug에서 사용할 변수를 보내줄 수 있다. ex) {pageTitle: "Home"}
+export const trending = (req, res) => { res.render("home", {pageTitle: "Home"})} 
+export const see = (req, res) => { res.render("watch", {pageTitle: "Watch"})}
+export const edit = (req, res) => { res.render("edit", {pageTitle: "Edit"})}
 export const search = (req, res) => { res.send("Search Videos")}
 export const upload = (req, res) => { res.send("Upload Videos")}
 export const deleteVideo = (req, res) => { res.send("Delete Videos")}

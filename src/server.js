c182f2b4f5a98d8704f07a1e0eb1bbccfcc1ad9d
Dean Kim <deankim1118@gmail.com>
2024-04-02 17:56:13 -0400

    Seperate Server.js to init.js
    Sever.js is for express()
    init.js is for database and listening server.

--- src/server.js
+++ src/server.js
@@ -1,5 +1,3 @@
-import "./db";
-import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
@@ -11,9 +9,6 @@ const logger =  morgan("dev");
 // Middleware는 request에 응답하는 함수가 아니라, request를 지연시키고 작업을 다음 함수로 넘겨주는 함수이다.
 // app.use를 사용하면 모든 Routes 에서 Middleware가 실행된다.
 
-
-const PORT = 4000;
-
 app.set("views", process.cwd() + "/src/views");
 app.set("view engine", "pug");
 app.use(logger);
@@ -22,6 +17,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-
-const handleListening = () => console.log(`Server is listening on port localhose:${PORT}`);
-app.listen(PORT, handleListening)
+export default app;

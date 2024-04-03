9858133e55b389e271b7449795cffd6f0c518040
Dean Kim <deankim1118@gmail.com>
2024-04-02 20:21:58 -0400

    Practice Mongoose Queries Try Catch

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,49 +1,34 @@
-let videos = [
-    {
-        title: "First Video", 
-        rating: 5, 
-        comments:2, 
-        createAt: "2 minutes ago",
-        views: 1,
-        id: 1,
-    }, 
-    {
-        title: "Second Video", 
-        rating: 4, 
-        comments:21, 
-        createAt: "30 minutes ago",
-        views: 49,
-        id: 2,
-    },
-    {
-        title: "Third Video", 
-        rating: 5, 
-        comments:2, 
-        createAt: "45 minutes ago",
-        views: 89,
-        id: 3,
-    },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => { 
-    // render 1번째 Parameter는 "home.pug" view engine의 파일 이름을 넣어준다.
-    // render 2번째에 Parameter에 pug에서 사용할 변수를 보내줄 수 있다. ex) {pageTitle: "Home", userDataObject}
-    return res.render("home", {pageTitle: "Home", videos});
+
+export const home = async (req, res) => { 
+    // await Video.find({}).then((videos) => {
+    //     console.log("video is available", videos);
+    //     return res.render("home", {pageTitle: "Home", videos});
+    // })
+    // .catch((err) => {
+    //     console.log("error : ", err);
+    // });
+    try {
+        const videos = await Video.find({});
+        // render 1번째 Parameter는 "home.pug" view engine의 파일 이름을 넣어준다.
+        // render 2번째에 Parameter에 pug에서 사용할 변수를 보내줄 수 있다. ex) {pageTitle: "Home", userDataObject}
+        return res.render("home", {pageTitle: "Home", videos});
+    } catch(err) {
+        return consolo.log("errors : ", err);
+    }
 }; 
 export const watch = (req, res) => { 
     const { id } = req.params;
-    const video = videos[id-1];
-    return res.render("watch", {pageTitle: `Watching ${video.title}`, video});
+    return res.render("watch", {pageTitle: `Watching `});
 };
 export const getEdit = (req, res) => {
     const { id } = req.params;
-    const video = videos[id-1]; 
-    return res.render("edit", {pageTitle: `Editing ${video.title}`, video});
+    return res.render("edit", {pageTitle: `Editing`});
 };
 export const postEdit = (req, res) => { 
     const { id } = req.params;
     const { title } = req.body;
-    videos[id-1].title = title;
     return res.redirect(`/videos/${id}`);
 };
 
@@ -53,15 +38,6 @@ export const getUpload = (req, res) => {
 export const postUpload = (req, res) => {
     const { title } = req.body;
     // here we will add a video to the videos array.
-    const newVideo = {
-        title: title, 
-        rating: 3, 
-        comments:21, 
-        createAt: "Just now",
-        views: 18,
-        id: videos.length + 1,
-    };
-    videos.push(newVideo);
-    console.log(videos);
+
     return res.redirect("/");
 };

362010411b3e7f59a7ae5611203d17eab9a5f63d
Dean Kim <deankim1118@gmail.com>
2024-04-03 15:04:56 -0400

    7-3. CRUD - Update Videos Data

--- src/models/Video.js
+++ src/models/Video.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose';
 
 const videoSchema = new mongoose.Schema({
     title: {type:String, required:true, trim:true, maxLength:20 },
-    description: {type:String, required:true, trim:true, minLength:20},
+    description: {type:String, required:true, trim:true, maxLength:80},
     createdAt: {type: Date, required: true, default: Date.now},
-    hashtags: [{type: String,trim:true}],
+    hashtags: [{type: String, trim:true}],
     meta: {
         views: {type: Number, required: true, default: 0},
         rating: {type: Number, required: true, default: 0},

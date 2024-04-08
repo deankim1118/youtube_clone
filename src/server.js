509d1f8da55f8bc00d3fe96950a85d9da43056d3
Dean Kim <deankim1118@gmail.com>
2024-04-07 22:27:18 -0400

    Store session into MongoStore

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import morgan from 'morgan';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
+import MongoStore from 'connect-mongo';
 import session from 'express-session';
 import { localsMiddleware } from './middlewares';
 
@@ -10,6 +11,7 @@ const app = express();
 const logger = morgan('dev');
 // Middleware는 request에 응답하는 함수가 아니라, request를 지연시키고 작업을 다음 함수로 넘겨주는 함수이다.
 // app.use를 사용하면 모든 Routes 에서 Middleware가 실행된다.
+// dotenv.config({ path: './.env' });
 
 app.set('views', process.cwd() + '/src/views');
 app.set('view engine', 'pug');
@@ -17,9 +19,15 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(
   session({
-    secret: 'Hello',
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false, // save only I create or modify the sessionsc
+    cookie: {
+      maxAge: 20000,
+    },
+    store: MongoStore.create({
+      mongoUrl: process.env.DB_URL,
+    }),
   })
 );
 app.use(localsMiddleware);

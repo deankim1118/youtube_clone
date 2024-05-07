43b96b97d93c7ac85e2a87737f5b42ddc4fe7bbd
Dean Kim <deankim1118@gmail.com>
2024-05-07 14:49:28 -0400

    19. Comments - dataset-id for Edit, Delete

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,9 +1,10 @@
 const videoContainer = document.getElementById('videoContainer');
 const form = document.getElementById('commentForm');
 
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector('.video__comments ul');
   const newComment = document.createElement('li');
+  newComment.dataset.id = id;
   newComment.className = 'video__comment';
   const span = document.createElement('span');
   span.innerText = ` ${text}`;
@@ -16,15 +17,16 @@ const handleSubmit = async (event) => {
   const textarea = form.querySelector('textarea');
   const text = textarea.value;
   const video = videoContainer.dataset.videoId;
-  const { status } = await fetch(`/api/videos/${video}/comment`, {
+  const response = await fetch(`/api/videos/${video}/comment`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ text }),
   });
-  if (status === 201) {
-    addComment(text);
+  if (response.status === 201) {
+    textarea.value = '';
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
   }
-  textarea.value = '';
 };
 if (form) {
   form.addEventListener('submit', handleSubmit);
